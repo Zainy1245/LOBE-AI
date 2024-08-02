b86ed6e6e5d4510718dd768ba377b71abec16297
Zain Hafeez <zainmirza3280@gmail.com>
2024-08-02 13:49:06 +0500

    all the project done just need to be submitted

--- scripts.js
+++ scripts.js
@@ -25,3 +25,18 @@ function toggleMenu() {
     const navbarNav = document.querySelector('.navbar-nav');
     navbarNav.classList.toggle('show');
 }
+//For help page 
+document.addEventListener('DOMContentLoaded', function() {
+    var faqs = document.querySelectorAll('.faq h2');
+    
+    faqs.forEach(function(faq) {
+        faq.addEventListener('click', function() {
+            var answer = this.nextElementSibling;
+            if (answer.style.display === 'block') {
+                answer.style.display = 'none';
+            } else {
+                answer.style.display = 'block';
+            }
+        });
+    });
+});
